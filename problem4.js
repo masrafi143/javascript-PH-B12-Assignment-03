ec97b0e576a696f73e7a1fa4cd99eb5d3492746f
masrafi143 <masrafiofficial143@gmail.com>
2025-08-03 23:52:19 +0600

    add solutions

---
+++ problem4.js
@@ -0,0 +1,12 @@
+/** Problem 04 - (Delete / Store) */
+var fileName= "pdfData.jpg";
+//write your code here
+if(fileName.includes('.pdf')){
+    console.log("Store");
+} else if(fileName.includes('.docx')){
+    console.log("Store");
+} else if(fileName.includes("#")){
+    console.log("Store");
+} else{
+    console.log("Delete");
+}
